0c18b56a96e5ed8af9ca58914a8fea34db3b2983
ErikBoesen <me@erikboesen.com>
2019-06-29 11:32:12 -0400

    Convert to JS syntax

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ const number = document.getElementById('number'),
 const expression = /^([A-Z]+)(\d)(\d+)([A-Z]+)?$/;
 number.onchange = function() {
     result = this.value.match(expression);
-    if (!result) {
-        return
+    if (result) {
+        let _, entryways, floor, suite, room = result;
+        response = 'Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '') +
+                ' ' + verbalize_list(entryways) + ', '
+                'is on floor ' + floor + ', and is suite #' + suite + ' on that floor.';
+        if (room) {
+            response += ' Your own room is room ' + room + ' within your suite.';
+        }
+        explanation.textContent = response;
     }
-    let _, entryways, floor, suite, room = result;
-    response = 'Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '') + ' ' + verbalize_list(entryways) + ', '
-            'is on floor {floor}, and is suite #{suite} on that floor.'
-        ).format(pluralize_entryway=,
-                 entryways=self.verbalize_list(entryways),
-                 floor=floor,
-                 suite=suite)
-        if room:
-            response += ' Your own room is room {room} within your suite.'.format(room=room)
 }
 
 function verbalize_list(items) {
     items = items.split()
-    if len(items) > 1:
-        last = items.pop()
-        items[items.length - 1] += ' and ' + last
-    return ', '.join(items)
+    if (items.length > 1) {
+        last = items.pop();
+        items[items.length - 1] += ' and ' + last;
+    }
+    return ', '.join(items);
 }
