1c33ba585d42248fc03ab100ae8e0c1a4ca7ea4d
ErikBoesen <me@erikboesen.com>
2019-06-29 11:39:22 -0400

    Use functional string building

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ number.oninput = function() {
             suite = result[3],
             room = result[4];
         console.log(entryways);
-        response = ('Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '') +
-                ' ' + verbalize_list(entryways) + ', '
-                'is on floor ' + floor + ', and is suite #' + suite + ' on that floor.');
+        response = ('Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '')
+                    + ' ' + verbalize_list(entryways) + ', '
+                    + 'is on floor ' + floor + ', and is suite #' + suite + ' on that floor.');
         if (room) {
             response += ' Your own room is room ' + room + ' within your suite.';
         }
