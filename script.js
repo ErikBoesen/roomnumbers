6cc58fa4eab46a7df303137a2e08ac2343b1e675
ErikBoesen <me@erikboesen.com>
2020-09-01 19:29:01 -0600

    Add building detection

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const number = document.getElementById('number'),
       explanation = document.getElementById('explanation');
 
+const BUILDINGS = {
+    'W': 'Welch Hall',
+    'F': 'Farnam Hall',
+    'D': 'Durfee Hall',
+    'L': 'Lawrance Hall',
+    'V': 'Vanderbilt Hall',
+    'LW': 'Lanman-Wright Hall',
+    'BK': 'Berkeley College',
+    'BR': 'Branford College',
+    'DC': 'Davenport College',
+    'ES': 'Ezra Stiles College',
+    'JE': 'Jonathan Edwards College',
+    'BF': 'Benjamin Franklin College',
+    'GH': 'Grace Hopper College',
+    'MC': 'Morse College',
+    'MY': 'Pauli Murray College',
+    'PC': 'Pierson College',
+    'SY': 'Saybrook College',
+    'SM': 'Silliman College',
+    'TD': 'Timothy Dwight College',
+    'TC': 'Trumbull College',
+    'BM': 'Bingham Hall',
+}
+
+
 function instruct() {
     explanation.textContent = '[Type a valid suite/room number to see an explanation.]';
 }
@@ -8,15 +33,26 @@ instruct();
 number.focus();
 
 
-const expression = /^([A-Z]+)(\d+)(\d)([A-Z]+)?$/;
+const expression = /^([A-Z]{1,2}-)?([A-Z]+)(\d+)(\d)([A-Z]+)?$/;
 number.oninput = function() {
     result = this.value.toUpperCase().match(expression);
+    console.log(result);
     if (result) {
-        let entryways = result[1],
-            floor = result[2],
-            suite = result[3],
-            room = result[4];
-        response = ('Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '')
+        let building_code = result[1],
+            entryways = result[2],
+            floor = result[3],
+            suite = result[4],
+            room = result[5];
+        response = '';
+        let building = undefined;
+        if (building_code) {
+            // Remove trailing hyphen
+            // TODO: figure out a way to do this with regex
+            building_code = building_code.substring(0, building_code.length - 1);
+            building = BUILDINGS[building_code];
+            response += 'You live in ' + building + ' (' + building_code + '). ';
+        }
+        response += ('Your suite can be accessed through entryway' + (entryways.length > 1 ? 's' : '')
                     + ' ' + verbalize_list(entryways) + ', '
                     + 'is on floor ' + floor + ', and is suite #' + suite + ' on that floor.');
         if (room) {
